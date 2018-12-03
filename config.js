51830e050761c7a4d7d995c507c17f7e80bb60cb
Claus Marxmeier <claus@marxmeier.de>
2018-12-03 22:23:02 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,30 +6,26 @@ module.exports = function () {
       {
         'name': 'Clientstatistik',
         'href': 'https://map.freifunk-rhein-sieg.net/netz/statistik/node/{NODE_ID}/',
-        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000026/node?panelId=1&var-node={NODE_ID}&from
-=now-1d&width=650&height=350&theme=light&_t={TIME}',
+        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000026/node?panelId=1&var-node={NODE_ID}&from=now-1d&width=650&height=350&theme=light&_t={TIME}',
         'title': 'Clientstatistik für {NODE_ID} - weiteren Statistiken'
       },
       {
         'name': 'Trafficstatistik',
         'href': 'https://map.freifunk-rhein-sieg.net/netz/statistik/node/{NODE_ID}/',
-        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000026/node?panelId=2&from=now-1d&var-node={N
-ODE_ID}&width=650&height=350&theme=light&_t={TIME}',
+        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000026/node?panelId=2&from=now-1d&var-node={NODE_ID}&width=650&height=350&theme=light&_t={TIME}',
         'title': 'Trafficstatistik für {NODE_ID} - weiteren Statistiken'
       },
       {
         'name': 'Systemlast',
         'href': 'https://map.freifunk-rhein-sieg.net/netz/statistik/node/{NODE_ID}/',
-        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000026/node?panelId=4&from=now-1d&var-node={N
-ODE_ID}&width=650&height=350&theme=light&_t={TIME}',
+        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000026/node?panelId=4&from=now-1d&var-node={NODE_ID}&width=650&height=350&theme=light&_t={TIME}',
         'title': 'Systemlast für {NODE_ID} - weiteren Statistiken'
       }
     ],
     'linkInfos': [
       {
         'name': 'Statistik für alle Links zwischen diese Knoten',
-        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/nvSNqoHmz/link?panelId=7&var-node={SOURCE_ID}
-&var-nodetolink={TARGET_ID}&from=now-1d&&width=650&height=350&theme=light&_t={TIME}',
+        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/nvSNqoHmz/link?panelId=7&var-node={SOURCE_ID}&var-nodetolink={TARGET_ID}&from=now-1d&&width=650&height=350&theme=light&_t={TIME}',
         'title': 'Linkstatistik des letzten Tages, min und max aller Links zwischen diesen Knoten'
       }
     ],
@@ -37,8 +33,7 @@ ODE_ID}&width=650&height=350&theme=light&_t={TIME}',
       {
         'name': 'Globale Statistik',
         'href': 'https://map.freifunk-rhein-sieg.net/netz/statistik',
-        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000028/globals?panelId=2&from=now-7d&&widt
-h=650&height=350&theme=light&_t={TIME}',
+        'image': 'https://grafana.freifunk-rhein-sieg.net/render/d-solo/000000028/globals?panelId=2&from=now-7d&&width=650&height=350&theme=light&_t={TIME}',
         'title': 'Globale Statistik - weiteren Statistiken'
       }
     ],
@@ -59,8 +54,7 @@ h=650&height=350&theme=light&_t={TIME}',
         'url': 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png',
         'config': {
           'maxZoom': 19,
-          'attribution': '&copy; Openstreetmap France | &copy; <a href="http://www.openstreetmap.org/copyright">OpenS
-treetMap</a>'
+          'attribution': '&copy; Openstreetmap France | &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }
       },
       {
@@ -68,8 +62,7 @@ treetMap</a>'
         'url': '//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
         'config': {
           'maxZoom': 20,
-          'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerog
-rid, IGN, IGP, UPR-EGP, and the GIS User Community'
+          'attribution': 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
         }
       },
     // Set a visible frame
